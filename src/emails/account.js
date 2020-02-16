80e10233ae228804663089867e0370702137d15c
slava sapir <slavas21@gmail.com>
2020-02-15 19:06:32 -0500

    commits

--- src/emails/account.js
+++ src/emails/account.js
@@ -1,13 +1,11 @@
 const sgMail = require('@sendgrid/mail')
 
-//const sendgridAPIKey = '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'
-
 sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
 const sendWelcomeEmail = (email, name) => {
     sgMail.send({
     to: email,
-    from: 'slavas21@gmail.com',
+    from: 'please provide your email!',
     subject: 'Thank you for joining us!',
     text: `Welcome to the app ${name}. Let me know how you get along with the app.`
 
