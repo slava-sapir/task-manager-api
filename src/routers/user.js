c0b9a764c32847b72c53129be1cf1d437c34ad95
slava sapir <slavas21@gmail.com>
2020-07-27 19:49:19 -0400

    changes

--- src/routers/user.js
+++ src/routers/user.js
@@ -65,7 +65,7 @@ router.post('/users/login', async (req, res) => {
 router.post('/unique', async (req, res) => {
    //controller
    try {
-        const user = await User.findByCredentials(req.body.name) 
+        const user = await User.find(req.body.name) 
         res.status(201).send({user});
      }
    catch(e) {
