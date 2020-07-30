a929eb579b0962d7a7289281f75488e10b74391a
slava sapir <slavas21@gmail.com>
2020-07-29 20:17:14 -0400

    changes to routes

--- src/routers/user.js
+++ src/routers/user.js
@@ -8,7 +8,7 @@ const { sendWelcomeEmail, sendCancelationEmail } = require('../emails/account')
 const router = new express.Router()
 
 router.all('*', function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", "http://localhost:4200");
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
     next()
   });
