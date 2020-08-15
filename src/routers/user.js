693159dcba83445581a7cd9e32527d0a3873cdb9
slava sapir <slavas21@gmail.com>
2020-08-15 18:47:55 -0400

    get avatar method added

--- src/routers/user.js
+++ src/routers/user.js
@@ -211,18 +211,30 @@ router.delete('/users/me/avatar', auth, async (req, res) => {
         res.status(200).send()
 })
 
-router.get('/users/:id/avatar', async (req, res) => {
-    try {
-         const user = await User.findById(req.params.id)
-         if(!user || !user.avatar){
-             throw new Error()
-         }
-          res.set('Content-Type', 'image/png')
-          res.send(user.avatar)
-    }
-    catch(e){
-        res.status(404).send()
-    }
+router.get('/users/me/avatar', auth, async (req, res) => {
+	
+	try{
+		if(!req.user || !req.user.avatar){
+        throw new Error()
+		}
+		res.set('Content-Type', 'image/png')
+	    res.send(req.user.avatar)
+	   
+	   } catch(e) {
+		res.status(404).send()
+		}
+	
+     // try {
+     // const user = await User.findById(req.params.id)
+     // if(!user || !user.avatar){
+     // throw new Error()
+     // }
+     // res.set('Content-Type', 'image/png')
+     // res.send(user.avatar)
+     // }
+     // catch(e){
+     // res.status(404).send()
+     // }
 })
 
 module.exports = router
