1405ec9ac845459410f247b1f927329e0234c712
slava sapir <slavas21@gmail.com>
2020-08-16 18:37:43 -0400

    change path-5

--- src/routers/user.js
+++ src/routers/user.js
@@ -211,25 +211,8 @@ router.delete('/users/me/avatar', auth, async (req, res) => {
         res.status(200).send()
 })
 
-router.get('/users/me/avatar', async(req, res) => {
+router.get('/users/:id/avatar', async(req, res) => {
 	
-	try {
-	 
-	     if(req.header('Authorization')){
-            const token = req.header('Authorization').replace('Bearer ', '')
-		    const decoded = jwt.verify(token, process.env.SECRET_KEY)
-            const user = await User.findOne({ _id: decoded._id, 'tokens.token': token })
-			res.set('Content-Type', 'image/png')
-			res.send(user.avatar)
-            //res.status(201).send({ authenticated: true, name: user.name })
-		 }
-		
-		 else res.status(200).send({ authenticated: false, name: null })
-          
-
-    } catch (e) {
-        res.status(404).send()
-    }	
 	
 	 // // // // // // // // // // try{
 		 // // // // // // // // // // if(!req.user || !req.user.avatar){
@@ -242,17 +225,17 @@ router.get('/users/me/avatar', async(req, res) => {
 		 // // // // // // // // // // res.status(404).send()
 	    // // // // // // // // // // }
 	
-     // try {
-     // const user = await User.findById(req.params.id)
-     // if(!user || !user.avatar){
-     // throw new Error()
-     // }
-     // res.set('Content-Type', 'image/png')
-     // res.send(user.avatar)
-     // }
-     // catch(e){
-     // res.status(404).send()
-     // }
+     try {
+      const user = await user.findbyid(req.params.id)
+      if(!user || !user.avatar){
+      throw new error()
+      }
+      res.set('Content-Type', 'image/png')
+      res.send(user.avatar)
+      }
+      catch(e){
+      res.status(404).send()
+      }
 })
 
 module.exports = router
